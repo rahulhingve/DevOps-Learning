cb3f3a5689118c01ffd9e444ede7e5d8ea0e6f5e
rahulhingve <rahulpawar2001.rp@gmail.com>
2025-07-02 22:06:55 +0530

    fixed some build and prisma deps error

--- week27-docker-compose/src/index.ts
+++ week27-docker-compose/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { PrismaClient } from "./generated/prisma";
+import { PrismaClient } from "../src/generated/prisma";
 const app = express();
 
 const prisma = new PrismaClient();
